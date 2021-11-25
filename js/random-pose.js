9e5b127cc6df7ef305840cb43c65e5686c2238ff
andrwyoung <yong.andrew11@gmail.com>
2021-11-25 13:55:18 -0800

    rearrange

--- js/random-pose.js
+++ js/random-pose.js
@@ -1,39 +1,87 @@
+function chooseRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function chooseRandomNull(label, array) {
+    var chosen_item = chooseRandom(array);
+    if (chosen_item == '') {
+        return '';
+    } else {
+        return "<br/>" + label + ": " + chosen_item;
+    }
+}
+
 function chooseWithProbability(words, probs) {
-    var i, sum = 0;
+    var i, total = 0;
     for (i = 0; i < probs.length; i++) {
-        sum += probs[i];
+        total += probs[i];
     }
-    if (sum != 100) {
-        console.log("ERROR: probabilities do not sum to 100. Probs = " + sum);
+
+    if (words.length != probs.length) {
+        console.log("ERROR: words length " + words.length + 
+                " and probs length "+ probs.length + " do not match");
     }
+    // console.log("total: " + total);
     
     var ar = [];
-    var probsums = 0
-    for (i = 0 ; i < probs.length-1 ; i++) {
-      probsums += (probs[i] / 100.0);
-      ar[i] = probsums;
+    var sum = 0
+    for (i = 0 ; i < probs.length; i++) {
+      sum += (probs[i] / total);
+    //   console.log("prob for " + words[i] + ": " + (probs[i] / total * 100));
+      ar[i] = sum;
     }
 
     var r = Math.random();
-    for (i = 0 ; i < ar.length && r >= ar[i] ; i++) ;
+    for (i = 0 ; i < ar.length - 1 && r >= ar[i] ; i++) ;
 
     return words[i];
 }
 
+function chooseWithProbabilityNull(label, words, probs) {
+    var chosen_item = chooseWithProbability(words, probs);
+    if (chosen_item == '') {
+        return '';
+    } else {
+        return "<br/>" + label + ": " + chosen_item;
+    }
+}
+
+function addPoseModifiers(pose) {
+    var actions, actProbs;
+    switch(pose) {
+        case 'standing':
+            actions = ['', 'weight on one foot', 'leaning on wall', 'on one foot', 'in ready position'];
+            actProbs = [3, 2, 1, 1, 1];
+            return chooseWithProbability(actions, actProbs);
+        case 'sitting':
+            actions = ['', 'in a chair', 'on the floor', 'on a couch', 'on a stool', 'at the desk'];
+            return chooseRandom(actions);
+        case 'lying down':
+            actions = ['', ' on the bed', 'on the side'];
+            return chooseRandom(actions);
+    }
+    return '';
+}
+
+
 function generatePose() {
-    // generate the pose
+    var genders = ['male', 'female'];
+    var gender = chooseRandom(genders);
+
+
     var poses = ['standing',
                 'sitting',
                 'running',
                 'jumping',
-                'eating',
                 'lying down',
                 'squatting',
                 'falling',
                 'punching',
-                'kicking'
+                'kicking',
+                'walking',
                 ];
-    var pose = "Pose: " + poses[Math.floor(Math.random() * poses.length)];
+    var chosenPose = chooseRandom(poses);
+    var pose = "<br/>Pose: " + chosenPose + " " + addPoseModifiers(chosenPose);
 
     var emotions = ['joyfully',
                 'fearfully',
@@ -45,37 +93,77 @@ function generatePose() {
                 'bored',
                 'surprised',
                 'disgusted',
+                'calmly',
+                'peacefully',
                 ];
-    var emotion = "Emotion: " + emotions[Math.floor(Math.random() * emotions.length)];
+    var emotion = "<br/>Emotion: " + chooseRandom(emotions);
 
 
 
-
-
-    // generate the angle of the camera
-    var angles = ['top angle (birds eye)',
-                'high angle',
-                'eye level',
-                'low angle',
-                'bottom angle (worms eye)',
+    var props = ['-',
+                'rifle',
+                'pistol',
+                'bat',
+                'pole',
+                'basketball',
+                'football',
+                'ramen',
+                'wand',
+                'book',
+                'laptop',
+                'knife'
                 ];
-    // we want a higher probability that "eye level" gets chosen
-    var angleProbs = [10, 20, 40, 20, 10]
-    var angle = "Camera Angle: " + chooseWithProbability(angles, angleProbs);
+    var propProbs = [10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    var prop = chooseWithProbabilityNull("Props", props, propProbs);
 
-    // generate angle of person relative to camera
-    var positions = ['front',
-                'back',
-                'side facing left',
-                'side facing right',
-                'front 3/4 facing left',
-                'front 3/4 facing left',
-                'back 3/4 facing left',
-                'back 3/4 facing left',
-                ];
-    var position = "Actor Angle: " + positions[Math.floor(Math.random() * positions.length)];
 
-    var final = pose + "<br/>" + emotion + "<br/><br/>" + angle;
+
+    var final = "Gender: " + gender + pose + emotion + 
+    "<br/>" + chooseView() +
+    "<br/>" + prop;
 
     document.getElementById("pose").innerHTML = final;
+}
+
+function chooseView() {
+    var angles = ['top angle (birds eye)',
+        'high angle',
+        'eye level',
+        'low angle',
+        'bottom angle (worms eye)',
+        ];
+    // we want a higher probability that "eye level" gets chosen
+    var angleProbs = [1, 2, 8, 2, 1]
+    var angle = "<br/>Camera Angle: " + chooseWithProbability(angles, angleProbs);
+
+
+    var positions = ['front',
+        'side',
+        'back',
+        ];
+    // we want a higher probability that "front" gets chosen
+    var posProbs = [10, 2, 1];
+    var position = "<br/>Actor Angle: " + chooseWithProbability(positions, posProbs);
+
+    var lookings = ['forward',
+        'to the side',
+        'behind',
+        'up',
+        'down'
+        ];
+    // we want a higher probability that "front" gets chosen
+    var lookProb = [10, 2, 1, 1, 1];
+    var looking = "<br/>Actor Looking: " + chooseWithProbability(lookings, lookProb);
+
+    return angle + position + looking;
+}
+
+function chooseClothing(gender) {
+    // var hat = [];
+    // var face = [];
+    // var glove = [];
+    // var shirt = [];
+    // var pants = [];
+    // var shoes = [];
+    // var addons = ['backpack', 'sachel', 'dog'];
 }
